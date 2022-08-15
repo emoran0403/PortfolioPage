5ef7102e7e4b11401e54be38b03819c18500297c
Eric Moran <emoran0403@gmail.com>
2022-08-15 16:39:34 -0400

    added text for just my type

--- src/client/Pages/ProjectPages/JustMyType.tsx
+++ src/client/Pages/ProjectPages/JustMyType.tsx
@@ -24,18 +24,27 @@ const JustMyType = (props: Types.JustMyTypeProps) => {
           <div className="card">
             <div className="card-body">
               <h3>Overview</h3>
-              <p>text here</p>
+              <p>
+                Just My Type is a typing game built entirely with HTML, CSS, and JS. The player types the sentences shown on the screen, restarting the sentence
+                if they make a mistake. The game also times the player and records their mistakes, showing a score at the end.
+              </p>
             </div>
             <hr></hr>
             <div className="card-body">
               <h3>Features</h3>
-              <p>text here</p>
+              <p>On-screen keyboard, highlighting buttons as they are pressed, and swapping to an uppercase keyboard when the shift button is held.</p>
+              <p>The player's time and mistakes are recorded, showing up as a score when the game is over.</p>
             </div>
             <hr></hr>
 
             <div className="card-body">
               <h3>Challenges</h3>
-              <p>text here</p>
+              <p>
+                There was a lot of conditional logic behind the scenes. Every key press had to be evaluated as it could have been the wrong key, the end of a
+                particular word, or the end of the sentence, and each of those changes had to update something elsewhere. This was where I found the value in
+                abstracting out repeated logic into smaller functions to clean up code. With clever contextual function names, the newly cleaned up code almost
+                read like high level instructions - a pattern I tried to implement in my future projects.
+              </p>
             </div>
             <hr></hr>
 
